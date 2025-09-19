fe0c9d32707f89efa5e2259f6756c7dad153ba78
Walmann <tovare@walmann.it>
2025-09-19 13:44:33 +0200

    Added CopyParty

---
+++ scripts/installCopyParty.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get install -y \
+    ffmpeg \
+    Pillow
+
+cd workspace
+wget https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py
+
+
+python3 ./copyparty-sfx.py
