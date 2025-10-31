b3a0f230c13d9eef385b76408a719a6a31556c15
Walmann <tovare@walmann.it>
2025-10-31 08:30:30 +0100

    Testing new CopyParty install process

--- installCopyParty.sh
+++ installCopyParty.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y \
-    ffmpeg \
-    python3-pil
+apt-get update && apt-get install tmux -y && tmux new-session -d -s copyparty 'curl -LsSf https://astral.sh/uv/install.sh | sh && source $HOME/.local/bin/env && uv tool run copyparty -p 8000 --allow-csrf' && tmux attach -t copyparty
 
-cd workspace
-wget https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py
 
-chmod 775 -R /workspace
 
-# python3 ./copyparty-sfx.py
+
+
+
+# apt-get update
+# apt-get install -y \
+#     ffmpeg \
+#     python3-pil
+
+# cd workspace
+# wget https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py
+
+# chmod 775 -R /workspace
+
+# # python3 ./copyparty-sfx.py
