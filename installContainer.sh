28566970edaa5d63865aa28bb035ea673f69ff67
Walmann <tovare@walmann.it>
2026-08-26 10:28:00 +0200

    Old installer clean

--- installContainer.sh
+++ installContainer.sh
@@ -11,37 +11,37 @@ border()
 }
 
 
-download_model_and_node_list(){
-    git clone "https://github.com/Walmann/Runpod-ComfyUI"
-    echo "Content of Runpod-ComfyUI/scripts:"
-    ls Runpod-ComfyUI/scripts
-    mv -f Runpod-ComfyUI/scripts/* /
+# download_model_and_node_list(){
+#     git clone "https://github.com/Walmann/Runpod-ComfyUI"
+#     echo "Content of Runpod-ComfyUI/scripts:"
+#     ls Runpod-ComfyUI/scripts
+#     mv -f Runpod-ComfyUI/scripts/* /
 
-    echo "Content of Runpod-ComfyUI/configs:"
-    ls Runpod-ComfyUI/configs
-    mkdir /workspace/configs/
-    mv -f Runpod-ComfyUI/configs/* /workspace/configs/
-    rm -rf Runpod-ComfyUI
+#     echo "Content of Runpod-ComfyUI/configs:"
+#     ls Runpod-ComfyUI/configs
+#     mkdir /workspace/configs/
+#     mv -f Runpod-ComfyUI/configs/* /workspace/configs/
+#     rm -rf Runpod-ComfyUI
 
-    echo "Moving to root folder"
+#     echo "Moving to root folder"
 
-    echo "Setting executables"
-    chmod +x /installComfyUI.sh
+#     echo "Setting executables"
+#     chmod +x /installComfyUI.sh
     
-    # # Nodes:
-    # border "Downloading Models and Node installer wizard"
-    # curl -L --progress-bar "https://raw.githubusercontent.com/Walmann/Runpod-ComfyUI/refs/heads/main/scripts/installNodes.sh" -o "/installNodes.sh" 
-    # # Models: 
-    # curl -L --progress-bar "https://raw.githubusercontent.com/Walmann/Runpod-ComfyUI/refs/heads/main/scripts/installModels.sh" -o "/installModels.sh" 
-    # # CloseUpShop Backup: 
-    # curl -L --progress-bar "https://raw.githubusercontent.com/Walmann/Runpod-ComfyUI/refs/heads/main/scripts/closeUpShop.sh" -o "/closeUpShop.sh" 
+#     # # Nodes:
+#     # border "Downloading Models and Node installer wizard"
+#     # curl -L --progress-bar "https://raw.githubusercontent.com/Walmann/Runpod-ComfyUI/refs/heads/main/scripts/installNodes.sh" -o "/installNodes.sh" 
+#     # # Models: 
+#     # curl -L --progress-bar "https://raw.githubusercontent.com/Walmann/Runpod-ComfyUI/refs/heads/main/scripts/installModels.sh" -o "/installModels.sh" 
+#     # # CloseUpShop Backup: 
+#     # curl -L --progress-bar "https://raw.githubusercontent.com/Walmann/Runpod-ComfyUI/refs/heads/main/scripts/closeUpShop.sh" -o "/closeUpShop.sh" 
 
-}
+# }
 
-if [ "$1" == "downloadOnly" ]; then
-    download_model_and_node_list
-    exit 1
-fi
+# if [ "$1" == "downloadOnly" ]; then
+#     download_model_and_node_list
+#     exit 1
+# fi
 
 border "Updating system"
 
@@ -51,9 +51,9 @@ apt-get -qq update && \
 
 echo "Finnished updating system"
 
-# Download Repository, but we only need files from a single folder...
-border "Downloading scripts"
-download_model_and_node_list
+# # Download Repository, but we only need files from a single folder...
+# border "Downloading scripts"
+# download_model_and_node_list
 
 border "Starting supervisor, to run multiple processes"
 supervisord -c ./service_script.conf
